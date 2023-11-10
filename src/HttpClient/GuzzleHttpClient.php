584da2b511941267adeb13f068fc0b57bd1f1263
vcheburkov <150165960+vcheburkov@users.noreply.github.com>
2023-11-10 18:39:01 +0300

    Update GuzzleHttpClient.php
    Updated $defaultAgent version to '7.8.0'

--- src/HttpClient/GuzzleHttpClient.php
+++ src/HttpClient/GuzzleHttpClient.php
@@ -86,7 +86,7 @@ class GuzzleHttpClient implements
      */
     private function appendUserAgent(array $options): array
     {
-        $defaultAgent = 'GuzzleHttp/' . Client::VERSION;
+        $defaultAgent = '7.8.0';
         if (extension_loaded('curl') && function_exists('curl_version')) {
             $curlVersion = \curl_version();
             if (\is_array($curlVersion)) {
